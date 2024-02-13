47b9c21c5033a0edd1077dbb0244e4ba35b9368a
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2024-02-13 07:51:11 +0700

    initial commit

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TicketRequest;
+
+class TicketController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store(TicketRequest $request)
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+}
---
+++ database/migrations/2024_02_13_004117_create_tickets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_ticket', 100)->nullable();
+            $table->string('nama', 100)->nullable();
+            $table->string('email', 60)->nullable();
+            $table->string('no_wa', 50)->nullable();
+            $table->string('jenis_pengaduan', 60)->nullable();
+            $table->text('deskripsi')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
